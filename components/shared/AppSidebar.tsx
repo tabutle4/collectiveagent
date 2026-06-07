91de72b9cf2f0607af10d1dadf1b5ca2ef39810e
Tara Butler <tabutle4@gmail.com>
2026-06-07 13:32:10 +0000

    fix: revert input css changes, overflow-x-hidden on content wrapper stops all horizontal bleed

--- components/shared/AppSidebar.tsx
+++ components/shared/AppSidebar.tsx
@@ -769,7 +769,7 @@ export default function AppSidebar({ children, logoUrl }: AppSidebarProps) {
           </p>
         </div>
 
-        <div className="px-4 md:px-6 pt-6 pb-20 md:pb-6">{children}</div>
+        <div className="px-4 md:px-6 pt-6 pb-20 md:pb-6 overflow-x-hidden">{children}</div>
       </div>
 
       {/* Mobile bottom nav bar - Back / Menu / Reload */}
